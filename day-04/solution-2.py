428b10afbb385d557e923c494e89b42c8038bfce
terrycojones <tcj25@cam.ac.uk>
2025-12-04 07:07:00 +0100

    day-04 golf

--- day-04/solution-2.py
+++ day-04/solution-2.py
@@ -3,12 +3,9 @@ from lib import read_data, removable
 data = read_data()
 total = 0
 
-while True:
-    if to_remove := removable(data):
-        total += len(to_remove)
-        for row, col in to_remove:
-            data[row][col] = "x"
-    else:
-        break
+while to_remove := removable(data):
+    total += len(to_remove)
+    for row, col in to_remove:
+        data[row][col] = "x"
 
 print(total)
